22c50fcf9d7c61b3ff2155c8771f6f4db3bf138f
Zack Wisti <zack@kirpi.me>
2020-01-31 15:36:40 +0000

    added visdom visualization

--- consumer/rmq-eeg-consumer.py
+++ consumer/rmq-eeg-consumer.py
@@ -6,6 +6,7 @@ import visdom
 import struct
 
 vis = visdom.Visdom();
+win = vis.line([0])
 startTime=0;
 
 def signal_handler(signal, frame):
@@ -15,37 +16,38 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 def unpackNameAndData(data):
-	fmt = "8s" + str(250-8) + "f"
+	fmt = "8s" + str(250) + "f"	
 	o = struct.unpack(fmt,data)
 	return o
 
 def nparray_callback(ch, method, props, body):
 	global startTime;	
-	out = list();
-	startIdx = 0;
-	chunkLen = 250*4+8;
-	stride = 250*4+8;
+	out = list();	
 	print("Total byte size = ", len(body))
-	while startIdx + stride < len(body):
-		d = body[startIdx:startIdx+chunkLen]
-		print("Size = ", len(d))
-		out = unpackNameAndData(d)
-		samples = out(1)
-		t = np.arange(startTime,startTime+len(samples))/len(samples)
-		vis.line(Y=samples,update='append')
+	
+	d = body
+	#print("Size = ", len(d))
+	out = unpackNameAndData(d)
+	#print(out)
+	print("starting at time ", startTime)
+	samples = list(out[1:]);
+	t = np.arange(startTime,startTime+len(samples))/len(samples)
+	vis.line(X=t,Y=samples,update='append',win=win)
+	startTime=startTime+50/250;
 	#o = unpackNameAndData(body);
-	#samples = o(1)	
+	#print(samples)
 	
 	
+credentials = pika.PlainCredentials("consumer","consumer")
 
-
-rmquser = os.environ['RABBITMQ_USERNAME']
-rmqpass = os.environ['RABBITMQ_PASSWORD']
-queue = "eeg"
-credentials = pika.PlainCredentials(rmquser,rmqpass)
-
-connection = pika.BlockingConnection(pika.ConnectionParameters('10.0.0.12',credentials=credentials))
+#rmquser = os.environ['RABBITMQ_USERNAME']
+#rmqpass = os.environ['RABBITMQ_PASSWORD']
+queue = "processing"
+args = dict()
+args['message-ttl']=10000
+connection = pika.BlockingConnection(pika.ConnectionParameters('54.201.180.173',credentials=credentials))
 channel = connection.channel()
+channel.queue_declare(queue=queue,arguments=args,durable = True)
 
 channel.basic_consume(queue=queue, on_message_callback=nparray_callback, auto_ack=True)
 
