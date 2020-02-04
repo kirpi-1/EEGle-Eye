0f664db68c4f02ae64536a6af7418f33451b67f8
Zack Wisti <zack@kirpi.me>
2020-02-04 09:28:17 +0000

    ?

--- producer/dummy-eeg-one-channel.py
+++ producer/dummy-eeg-one-channel.py
@@ -52,7 +52,7 @@ while(True):
 	cycleTime = t % fullCycle - fullCycle/2
 	signal = signal + 2*np.cos(2*np.pi*t*freqs[1])*(cycleTime/fullCycle)		
 	signal = signal / len(freqs); #normalize
-	
+	signal = np.arange(0,250,dtype=np.float)
 	print("    [x] Sending floats from {} to {}".format(t[0],t[-1]))
 	#local plotting of signal
 	plotTime[0:750] = plotTime[250:];
