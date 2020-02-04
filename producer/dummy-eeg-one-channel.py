02ca47c886a2e5546ef2e9862ddf8ebfeb45ee72
Zack Wisti <zack.wisti@gmail.com>
2020-02-04 00:19:12 -0800

    edits

--- producer/dummy-eeg-one-channel.py
+++ producer/dummy-eeg-one-channel.py
@@ -67,6 +67,6 @@ while(True):
 	channel.basic_publish(exchange='',
 						routing_key=routing_key,
 						properties=props,
-						body=packNameAndData("user1",signal))
+						body=frame)
 	startTime = startTime+1
 	time.sleep(1)
