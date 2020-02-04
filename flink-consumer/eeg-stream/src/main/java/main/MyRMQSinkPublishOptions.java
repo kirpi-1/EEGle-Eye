967ed05a24fe7720df137fab6f7ef264d07a92cc
Zack Wisti <zack.wisti@gmail.com>
2020-02-04 02:21:02 -0800

    made classes use EEGHeader instead of String

--- flink-consumer/eeg-stream/src/main/java/main/MyRMQSinkPublishOptions.java
+++ flink-consumer/eeg-stream/src/main/java/main/MyRMQSinkPublishOptions.java
@@ -4,19 +4,21 @@ import org.apache.flink.streaming.connectors.rabbitmq.RMQSinkPublishOptions;
 import com.rabbitmq.client.AMQP.BasicProperties;
 import org.apache.flink.api.java.tuple.Tuple2;
 
-public class MyRMQSinkPublishOptions implements RMQSinkPublishOptions<Tuple2<String, float[]>> {
+import eegstreamerutils.EEGHeader;
+
+public class MyRMQSinkPublishOptions implements RMQSinkPublishOptions<Tuple2<EEGHeader, float[]>> {
 	@Override
-	public String computeExchange(Tuple2<String, float[]> frame){
+	public String computeExchange(Tuple2<EEGHeader, float[]> frame){
 		return "";
 	}
 	@Override
-	public BasicProperties computeProperties(Tuple2<String, float[]> frame){
+	public BasicProperties computeProperties(Tuple2<EEGHeader, float[]> frame){
 		return new BasicProperties.Builder()
 				.deliveryMode(2)//durable
 				.build();
 	}
 	@Override
-	public String computeRoutingKey(Tuple2<String, float[]> frame){
+	public String computeRoutingKey(Tuple2<EEGHeader, float[]> frame){
 		return "processing";
 	}
 	
