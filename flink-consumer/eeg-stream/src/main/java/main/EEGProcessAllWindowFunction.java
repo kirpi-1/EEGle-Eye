a76e4a3aaed6241f3197b4fcc085ed3c93540186
Zack Wisti <zack@kirpi.me>
2020-02-05 08:09:32 +0000

    1

--- flink-consumer/eeg-stream/src/main/java/main/EEGProcessAllWindowFunction.java
+++ flink-consumer/eeg-stream/src/main/java/main/EEGProcessAllWindowFunction.java
@@ -21,7 +21,7 @@ public class EEGProcessAllWindowFunction
 	public void process(Context context, Iterable<Tuple3<Integer,EEGHeader,float[]>> frames, 
 						Collector<Tuple2<EEGHeader, float[]>> out) {
 		long numMsgs = frames.spliterator().getExactSizeIfKnown();		
-		log.info(String.format("Received %d messages!", numMsgs));
+//		log.info(String.format("Received %d messages!", numMsgs));
 		float[] data= new float[0];
 		int lastIdx = 0; //remembers the last index that was processed
 		int frameLen = 0; //length of each frame (should be 250 during testing)
@@ -30,7 +30,6 @@ public class EEGProcessAllWindowFunction
 		// get the actual float data from each frame
 		// and combine it into one long array that can be subsectioned
 		for(Tuple3<Integer,EEGHeader,float[]> frame: frames){
-			
 			//grab data (for convenience)
 			float[] frameData = frame.f2;
 			//grab the last index (the new starting point)
@@ -38,8 +37,9 @@ public class EEGProcessAllWindowFunction
 			if(idx==0){
 				lastIdx = frame.f0;
 				frameLen = frameData.length;
-				header = frame.f1;
-			}						
+				//header = frame.f1;
+			}
+			header = frame.f1;
 			//log.info(String.format("    [x] %d numbers from %d to %d",i.length,i[0],i[i.length-1]));
 			//get the current lenth of the data array
 			int prevLength = data.length;
@@ -48,19 +48,29 @@ public class EEGProcessAllWindowFunction
 			data = Arrays.copyOf(data, data.length+frameData.length);
 			System.arraycopy(frameData, 0, data,prevLength,frameData.length);
 			idx++;
+			log.info("=====================================");
+		    log.info(String.format("user name   : %s", header.user_name));
+        	log.info(String.format("frame number: %d", header.frame_number));
+        	log.info(String.format("timestamp   : %d", header.time_stamp));
+        	log.info(String.format("ML Model    : %s", header.ML_model));
+        	log.info(String.format("samplingrate: %d", header.sampling_rate));
+        	log.info(String.format("num channels: %d", header.num_channels));
+        	log.info(String.format("num samples : %d", header.num_samples));
+        	log.info(String.format("    %s",header.channel_names));
+        	log.info("=====================================");
 			//System.out.println(String.format("Processing: %s",header));
 		}
 		if(numMsgs==1)
 			frameLen=0;
 		
-		log.info(String.format("  [x] %d numbers",data.length));
+//		log.info(String.format("  [x] %d numbers",data.length));
 		// create a sliding window along the data and push that to stream out
 		int startIdx=lastIdx;
 		int chunkLength = 250;
 		int stride = 50;
 		int chunkNum=0;
 		while(startIdx + chunkLength < data.length){
-			log.info(String.format("    [x] Indices: %d to %d",startIdx, startIdx+chunkLength));
+			//log.info(String.format("    [x] Indices: %d to %d",startIdx, startIdx+chunkLength));
 			float[] tmp = Arrays.copyOfRange(data,startIdx,startIdx+chunkLength);
 			out.collect(new Tuple2(header,tmp));
 			//log.info(String.format("        [o] pushing chunk %d",chunkNum));
