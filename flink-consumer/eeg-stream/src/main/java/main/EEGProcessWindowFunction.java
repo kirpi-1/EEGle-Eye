0582abbbbe1e71f5bae36d2413210c2c913108e1
Zack Wisti <zack@kirpi.me>
2020-02-06 16:34:55 -0800

    added processwindowfunction

--- flink-consumer/eeg-stream/src/main/java/main/EEGProcessWindowFunction.java
+++ flink-consumer/eeg-stream/src/main/java/main/EEGProcessWindowFunction.java
@@ -2,7 +2,7 @@ package eegProcess;
 
 import java.util.Arrays;
 
-import org.apache.flink.streaming.api.functions.windowing.ProcessAllWindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
 import org.apache.flink.streaming.api.functions.windowing.ProcessAllWindowFunction.Context;
