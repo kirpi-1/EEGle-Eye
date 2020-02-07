caebc547ea9122eed591ba9f26b31610b9ccbc7c
Zack Wisti <zack@kirpi.me>
2020-02-06 16:11:33 -0800

    why ignored

--- flink-consumer/eeg-stream/src/main/java/main/UserKeySelector.java
+++ flink-consumer/eeg-stream/src/main/java/main/UserKeySelector.java
@@ -17,7 +17,7 @@ import eegstreamerutils.EEGHeader;
 
 class UserKeySelector implements KeySelector<Tuple3<Integer, EEGHeader, float[]>, String>{
 	@Override
-	public getKey(Tuple<Integer, EEGHeader, float []> frame){
+	public String getKey(Tuple<Integer, EEGHeader, float []> frame){
 		return frame.f1.user_name;
 	}
 }
