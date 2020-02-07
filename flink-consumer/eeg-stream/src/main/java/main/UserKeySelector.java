9327abd80da316ed8253b0082155825933fa513b
Zack Wisti <zack@kirpi.me>
2020-02-06 16:13:20 -0800

    fixing public issue

--- flink-consumer/eeg-stream/src/main/java/main/UserKeySelector.java
+++ flink-consumer/eeg-stream/src/main/java/main/UserKeySelector.java
@@ -15,7 +15,7 @@ import com.google.gson.JsonParser;
 
 import eegstreamerutils.EEGHeader;
 
-class UserKeySelector implements KeySelector<Tuple3<Integer, EEGHeader, float[]>, String>{
+public class UserKeySelector implements KeySelector<Tuple3<Integer, EEGHeader, float[]>, String>{
 	@Override
 	public String getKey(Tuple<Integer, EEGHeader, float []> frame){
 		return frame.f1.user_name;
