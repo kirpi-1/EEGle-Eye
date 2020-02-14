8142165e365ef221550dc77f2a6faf5d2330738a
Zack Wisti <zack.wisti@gmail.com>
2020-02-13 23:29:46 -0800

    timestamping flink

--- flink-consumer/eeg-stream/src/main/java/main/EEGProcessWindowFunction.java
+++ flink-consumer/eeg-stream/src/main/java/main/EEGProcessWindowFunction.java
@@ -97,9 +97,8 @@ public class EEGProcessWindowFunction
 		int numChannels = header.num_channels; // need number of channels for proper spacing		
 		int chunkNum=0;
 		int windowLength = (int)(windowLengthInSec*header.sampling_rate);
-		int strideLength = (int)((1-windowOverlap)*windowLengthInSec*header.sampling_rate);
-		System.out.println(String.format("%f, %f, %d, %f",1.0-windowOverlap, windowLengthInSec,header.sampling_rate, windowLengthInSec*header.sampling_rate));
-		System.out.println(String.format("%f\t%d",((1-windowOverlap)*windowLengthInSec*header.sampling_rate),(int)((1-windowOverlap)*windowLengthInSec*header.sampling_rate)));
+		int strideLength = (int)((1-windowOverlap)*windowLengthInSec*header.sampling_rate + .5);
+		System.out.println(String.format("%f, %f, %d, %f",1.0-windowOverlap, windowLengthInSec,header.sampling_rate, windowLengthInSec*header.sampling_rate));		
 		while(startIdx + windowLength*numChannels < data.length){
 			System.out.println(String.format("windowLen: %d\tstrideLen: %d\tstartIdx: %d\tendIdx: %d",
 			windowLength, strideLength,startIdx,startIdx+windowLength*numChannels));
