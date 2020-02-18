8c08109777cbfeb12b88e2d209d70e30b66e4ce5
Zack Wisti <zack.wisti@gmail.com>
2020-02-18 01:44:48 -0800

    changed to sos filter for butterworth

--- processor/standard-processor.py
+++ processor/standard-processor.py
@@ -10,49 +10,29 @@ import RMQUtils
 from scipy.signal import butter, lfilter, freqz
 import logging
 
+# sanity check to quit on CTRL+C
 def signal_handler(signal, frame):
 	print("\nprogram exiting gracefully")
 	sys.exit(0)
-
 signal.signal(signal.SIGINT, signal_handler)
 
+# get generic parser from utils module
 parser = RMQUtils.getParser()
+# override default argument and add argument
 parser.set_defaults(RMQuser='processor', RMQpassword='processor')
 parser.add_argument("-i","--RMQinput-queue",default="processing")
 args = parser.parse_args()
 
 HIGHPASS_CUTOFF = 1
-BANDSTOP_FREQ = np.array([59, 61])
+BANDSTOP_FREQ = 60
 
-# connection settings
-cred = pika.PlainCredentials(args.RMQuser,args.RMQpassword)
-rmqIP = args.RMQhost
-rmqExchange = args.RMQexchange
-routing_key=args.RMQqueue
-in_queue = args.RMQinput_queue
-
-
-params = pika.ConnectionParameters(	host=rmqIP, \
-									port=args.RMQport,\
-									credentials=cred, \
-									virtual_host=args.RMQvhost)
-connection = pika.BlockingConnection(params)
-channel = connection.channel()
-channel.queue_declare(queue=in_queue,durable = True, passive=True)
-
+# turn on logger
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
-def create_butterworth(cutoff, fs, order=5,type='lowpass'):
-	nyq = 0.5 * fs
-	normal_cutoff = cutoff / nyq
-	b, a = butter(order, normal_cutoff, btype=type, analog=False)
-	return b, a
-
 def butterworth_filter(data, cutoff, fs, type='lowpass', order=5):
-	b, a = create_butterworth(cutoff, fs, order=order,type=type)
-	y = lfilter(b, a, data, axis=0)
+	sos = butter(order, cutoff, btype=type, output='sos',fs=fs)	
+	y = signal.sosfilt(sos, data, axis=0)
 	return y
 
 def nparray_callback(ch, method, props, body):
@@ -61,9 +41,10 @@ def nparray_callback(ch, method, props, body):
 	header, data = unpackHeaderAndData(body)
 	#get channel number for time/TIME
 	timeChan, eeg = splitTimeAndEEG(header, data)
-	# lowpass first
-	eeg = butterworth_filter(eeg,BANDSTOP_FREQ,header['sampling_rate'],type='bandstop')
-	# then highpass
+	# bandstop 60hz and harmonics
+	for f in np.arange(BANDSTOP_FREQ,header['sampling_rate']/2,BANDSTOP_FREQ):
+		eeg = butterworth_filter(eeg,[f-1,f+1],header['sampling_rate'],type='bandstop')
+	# then highpass to remove <1Hz signal
 	eeg = butterworth_filter(eeg,HIGHPASS_CUTOFF,header['sampling_rate'],type='highpass')
 	# then fft
 	eegfft = np.absolute(np.fft.fft(eeg,axis=0))
@@ -77,6 +58,23 @@ def nparray_callback(ch, method, props, body):
 						routing_key="ml."+header['ML_model'],
 						body=frame,
 						mandatory=True)#properties=props,
+
+
+# connection settings
+cred = pika.PlainCredentials(args.RMQuser,args.RMQpassword)
+rmqIP = args.RMQhost
+rmqExchange = args.RMQexchange
+routing_key=args.RMQqueue
+in_queue = args.RMQinput_queue
+params = pika.ConnectionParameters(	host=rmqIP, \
+									port=args.RMQport,\
+									credentials=cred, \
+									virtual_host=args.RMQvhost)
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+channel.queue_declare(queue=in_queue,durable = True, passive=True)
+
+
 
 channel.basic_consume(queue=in_queue, on_message_callback=nparray_callback, auto_ack=True)
 print(' [*] Connected to:\n\t{}\n\t{}\n [*] as {}. Waiting for messages. To exit press CTRL+C'.format(":".join([rmqIP,str(args.RMQport)]),":".join([args.RMQvhost,args.RMQexchange,in_queue]), args.RMQuser))
